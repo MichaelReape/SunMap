02c390e78986595b8f9c96d938829226518c6d3c
Michael Reape <michaelreape@gmail.com>
2025-08-19 22:39:57 +0100

    Initial spring boot setup

---
+++ sunmap/src/main/java/com/sunmap/app/SunmapApplication.java
@@ -0,0 +1,13 @@
+package com.sunmap.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunmapApplication.class, args);
+	}
+
+}
